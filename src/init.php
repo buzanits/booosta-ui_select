0169c62e42a6fb33de4c6e290b4df395ab979205
Peter Buzanits <p.buzanits@epb.at>
2022-01-14 00:13:26 +0100

    work on usersystem

--- src/init.php
+++ src/init.php
@@ -8,8 +8,8 @@ trait webapp
   protected function preparse_ui_select()
   {
     if($this->moduleinfo['ui_select']):
-      $this->add_includes("<script type='text/javascript' src='vendor/grimmlink/selectize/dist/js/standalone/selectize.js'></script>
-<link rel='stylesheet' type='text/css' href='vendor/grimmlink/selectize/dist/css/selectize.default.css' media='screen' />");
+      $this->add_includes("<script type='text/javascript' src='{$this->base_dir}vendor/grimmlink/selectize/dist/js/standalone/selectize.js'></script>
+<link rel='stylesheet' type='text/css' href='{$this->base_dir}vendor/grimmlink/selectize/dist/css/selectize.default.css' media='screen' />");
     endif;
   }
 }
